9a13df76a5f2a124d4da724d9a49251011bf9ee9
Luis Miguel <luismiguelossaarias05@gmail.com>
2020-10-30 12:58:52 -0500

    Fifth upload

--- src/model/OtherPatient.java
+++ src/model/OtherPatient.java
@@ -2,7 +2,31 @@ package model;
 
 public class OtherPatient extends PrepaidMedicinePatient{
 
-	public OtherPatient(){
+	//Attribute
 
+	private String nomOrganization;
+	private String phone;
+
+	public OtherPatient(String name, String lastName, int age, String id, String numAffiliation, String nomOrganization, String phone){
+
+		super(name,lastName,age,id,numAffiliation);
+		this.nomOrganization = nomOrganization;
+		this.phone = phone;
+	}
+
+	public String getNomOrganization(){
+		return nomOrganization;
+	}
+
+	public void setNomOrganization(String nomOrganization){
+		this.nomOrganization = nomOrganization;
+	}
+
+	public String getPhone(){
+		return phone;
+	}
+
+	public void setPhone(String phone){
+		this.phone = phone;
 	}
 }
--- src/model/SURAPatient.java
+++ src/model/SURAPatient.java
@@ -2,7 +2,33 @@ package modedl;
 
 public class SURAPatient extends PrepaidMedicinePatient{
 
-	public SURAPatient(){
+	//Attribute
 
+	private String minPolicy;
+	private String maxPolicy;
+
+	//Methods
+
+	public SURAPatient(String name, String lastName, int age, String id, String numAffiliation, String minPolicy, String maxPolicy){
+
+		super(name,lastName,age,id);
+		this.minPolicy = minPolicy;
+		this.maxPolicy = maxPolicy;
+	}
+
+	public String getMinPolicy(){
+		return minPolicy;
+	}
+
+	public void setMinPolicy(String minPolicy){
+		this.minPolicy = minPolicy;
+	}
+
+	public String getMaxPolicy(){
+		return maxPolicy;
+	}
+
+	public void setMaxPolicy(String maxPolicy){
+		this.maxPolicy = maxPolicy;
 	}
 }
